ccca6e0335e2004337e597d48283b53e4e856b9f
Xinyi. C <xinyichen@gwu.edu>
2020-02-07 17:32:03 +0000

    Update main.py

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ if __name__ == '__main__':
     while True:
         cntr = 1
         is_found = False
-        query_data = input(u'\n输入您要查询的车次/车牌/航班号/场所名（如需退出，按下Ctrl+C）：')
+        query_data = input(u'\n输入您要查询的车次/车牌/航班号/场所名/站名/地名（如需退出，按下Ctrl+C）：')
         for itinerary in data['data']:
-            if str(itinerary['t_no']).lower().find(query_data.lower()) is not -1:
+            if str(itinerary['t_no']).lower().find(query_data.lower()) is not -1 or str(itinerary['t_pos_start']).lower().find(query_data.lower()) is not -1 or str(itinerary['t_pos_end']).lower().find(query_data.lower()) is not -1:
                 print('\n---------------------------------------------------------------------------------\n' + u'|记录' + str(cntr) + '|\n\n' + u'日期：' + str(itinerary['t_date']) + '\n' + u'车次/车牌/航班号/场所名：' + str(itinerary['t_no']) + '\n' + u'出行类型：' + str(itinerary['t_type']) + '\n' + u'车厢：' + str(itinerary['t_no_sub']) + '\n' + u'出发站：' + str(itinerary['t_pos_start']) + '\n' + u'到达站：' + str(itinerary['t_pos_end']) + '\n' + u'出行描述：' + str(itinerary['t_memo']) + '\n' + u'线索来源：' + str(itinerary['source']))
                 cntr += 1
                 is_found = True
         if is_found is False:
             print('\n恭喜您，目前没有相关记录！\n')
         else:
-            print('\n---------------------------------------------------------------------------------\n共找到' + str(cntr - 1) + '条相关记录。')
+            print('\n=================================================================================\n共找到' + str(cntr - 1) + '条相关记录。\n=================================================================================')

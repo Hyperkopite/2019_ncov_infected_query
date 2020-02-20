b1f66dfd9d1718a18e2e536081346bad81757e40
Xinyi. C <xinyichen@gwu.edu>
2020-02-20 18:24:39 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-# encoding=utf-8
+# coding=utf-8
 import requests
+import time
 
 
 def get_json(url_json):
@@ -8,23 +9,28 @@ def get_json(url_json):
 
 
 if __name__ == '__main__':
-    travel_type = ['飞机', '火车', '地铁', '长途客车/大巴', '公交车', '出租车', '轮船', '其他公共场所']
+    travel_type = [u'飞机', u'火车', u'地铁', u'长途客车/大巴', u'公交车', u'出租车', u'轮船', u'其他公共场所']
     url = 'http://2019ncov.nosugartech.com/data.json'
-    print(u'正在联网更新数据，请稍等...')
+    latest_time = ''
+    print(u'[!] 正在联网更新数据，请稍等...')
     data = get_json(url)
-    print(u'数据更新完毕。')
     for itinerary in data['data']:
+        latest_time = max(latest_time, itinerary['updated_at'])
         itinerary['t_type'] = travel_type[itinerary['t_type'] - 1]
+    print(u'[+] 数据更新完毕。')
+    time.sleep(0.5)
+    print('[+] 当前共有' + str(len(data['data'])) + u'条数据。数据更新时间：' + str(latest_time))
+
     while True:
         cntr = 1
         is_found = False
-        query_data = input(u'\n输入您要查询的车次/车牌/航班号/场所名/站名/地名（如需退出，按下Ctrl+C）：')
+        query_data = input(u'\n[?] 输入您要查询的车次/车牌/航班号/场所名/站名/地名（如需退出，按下Ctrl+C）：')
         for itinerary in data['data']:
             if str(itinerary['t_no']).lower().find(query_data.lower()) is not -1 or str(itinerary['t_pos_start']).lower().find(query_data.lower()) is not -1 or str(itinerary['t_pos_end']).lower().find(query_data.lower()) is not -1:
                 print('\n---------------------------------------------------------------------------------\n' + u'|记录' + str(cntr) + '|\n\n' + u'日期：' + str(itinerary['t_date']) + '\n' + u'车次/车牌/航班号/场所名：' + str(itinerary['t_no']) + '\n' + u'出行类型：' + str(itinerary['t_type']) + '\n' + u'车厢：' + str(itinerary['t_no_sub']) + '\n' + u'出发站：' + str(itinerary['t_pos_start']) + '\n' + u'到达站：' + str(itinerary['t_pos_end']) + '\n' + u'出行描述：' + str(itinerary['t_memo']) + '\n' + u'线索来源：' + str(itinerary['source']))
                 cntr += 1
                 is_found = True
         if is_found is False:
-            print('\n恭喜您，目前没有相关记录！\n')
+            print(u'\n[V] 恭喜您，目前没有相关记录！\n')
         else:
-            print('\n---------------------------------------------------------------------------------\n共找到' + str(cntr - 1) + '条相关记录。\n=================================================================================\n')
+            print(u'\n---------------------------------------------------------------------------------\n共找到' + str(cntr - 1) + u'条相关记录。\n=================================================================================\n')

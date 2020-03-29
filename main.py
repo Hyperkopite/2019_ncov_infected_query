c71e3132401f3e12876c2d2f7f5d421359a92e61
MjAxOS43LjI2 <anfieldk18@gmail.com>
2020-03-30 03:09:09 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ url = 'http://2019ncov.nosugartech.com/data.json'
 latest_time = ''
 cntr = 1
 is_found = False
+delim_line_single = '----------------------------------------------------------------------------------------------------'
+delim_line_double = '===================================================================================================='
 
 
 def get_json(url_json):
@@ -16,9 +18,8 @@ def get_json(url_json):
 
 def print_res():
     global cntr, is_found
-    print('\n---------------------------------------------------------------------------------\n' + u'|记录' + str(cntr) + '|\n\n' + u'日期：' + str(itinerary['t_date']) + '\n' + u'车次/车牌/航班号/场所名：' + str(itinerary['t_no']) + '\n' + u'出行类型：' + str(itinerary['t_type']) + '\n' + u'车厢：' + str(itinerary['t_no_sub']) + '\n' + u'出发站：' + str(itinerary['t_pos_start']) + '\n' + u'到达站：' + str(itinerary['t_pos_end']) + '\n' + u'出行描述：' + str(itinerary['t_memo']) + '\n' + u'线索来源：' + str(itinerary['source']))
+    print('\n' + delim_line_single + '\n' + u'|记录' + str(cntr) + '|\n\n' + u'日期：' + str(itinerary['t_date']) + '\n' + u'车次/车牌/航班号/场所名：' + str(itinerary['t_no']) + '\n' + u'出行类型：' + str(itinerary['t_type']) + '\n' + u'车厢：' + str(itinerary['t_no_sub']) + '\n' + u'出发站：' + str(itinerary['t_pos_start']) + '\n' + u'到达站：' + str(itinerary['t_pos_end']) + '\n' + u'出行描述：' + str(itinerary['t_memo']) + '\n' + u'线索来源：' + str(itinerary['source']))
     cntr += 1
-    is_found = True
 
 
 if __name__ == '__main__':
@@ -29,19 +30,22 @@ if __name__ == '__main__':
         itinerary['t_type'] = travel_type[itinerary['t_type'] - 1]
     print(u'[+] 数据更新完毕。')
     time.sleep(0.5)
-    print('[+] 当前共有' + str(len(data['data'])) + u'条数据。数据更新时间：' + str(latest_time))
+    print(u'[+] 当前共有' + str(len(data['data'])) + u'条数据。数据更新时间：' + str(latest_time))
 
     while True:
         cntr = 1
-        is_found = False
-        query_data = input(u'\n[?] 输入您要查询的车次/车牌/航班号/场所名/站名/地名/时间（时间格式：yyyy-mm-dd/mm-dd；如需退出，按下Ctrl+C）：')
+        is_found = True
+        print(u'\n[!] 支持同时满足多个条件的查询，条件用空格分隔开，时间格式：yyyy-mm-dd/mm-dd；如需退出，按下Ctrl + C')
+        query_data = input(u'[?] 输入您要查询的车次/车牌/航班号/场所名/站名/地名/时间/交通工具：').split(' ')
         for itinerary in data['data']:
-            if query_data.find('-') is not -1:
-                if str(itinerary['t_date']).find(query_data) is not -1:
-                    print_res()
-            elif str(itinerary['t_no']).lower().find(query_data.lower()) is not -1 or str(itinerary['t_pos_start']).lower().find(query_data.lower()) is not -1 or str(itinerary['t_pos_end']).lower().find(query_data.lower()) is not -1:
+            is_found = True
+            for query in query_data:
+                if str(itinerary['t_no']).lower().find(query.lower()) is -1 and str(itinerary['t_pos_start']).lower().find(query.lower()) is -1 and str(itinerary['t_pos_end']).lower().find(query.lower()) is -1 and str(itinerary['t_date']).find(query) is -1 and str(itinerary['t_type']).find(query) is -1:
+                    is_found = False
+                    break
+            if is_found is True:
                 print_res()
-        if is_found is False:
-            print(u'\n[V] 恭喜您，目前没有相关记录！\n')
+        if cntr == 1:
+            print(u'\n[v] 恭喜您，目前没有相关记录！\n')
         else:
-            print(u'\n---------------------------------------------------------------------------------\n共找到' + str(cntr - 1) + u'条相关记录。\n'u'=================================================================================\n')
+            print(delim_line_single + u'\n共找到' + str(cntr - 1) + u'条相关记录。\n' + delim_line_double + '\n')
